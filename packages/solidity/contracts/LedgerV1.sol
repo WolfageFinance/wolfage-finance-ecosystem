7456687a1e66ddf4a16a6528c97361f509b71093
Togo Cugu <togo.wolfage.finance@protonmail.com>
2020-09-22 13:41:46 +0530

    Implemented access control in LedgerV1 with pausable functionality.

---
+++ packages/solidity/contracts/LedgerV1.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.6.2;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+
+contract LedgerV1 is AccessControl, Pausable {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+
+    uint256 public constant version = 1;
+
+    event EtherReceived(address indexed from, uint256 value);
+
+    constructor(address owner) public {
+        _setupRole(DEFAULT_ADMIN_ROLE, owner);
+        _setupRole(PAUSER_ROLE, owner);
+        _setupRole(MANAGER_ROLE, owner);
+    }
+
+    receive() external payable {
+        emit EtherReceived(msg.sender, msg.value);
+    }
+
+    function pause() public virtual {
+        require(
+            hasRole(PAUSER_ROLE, _msgSender()),
+            "LedgerV1: must have pauser role to pause"
+        );
+        _pause();
+    }
+
+    function unpause() public virtual {
+        require(
+            hasRole(PAUSER_ROLE, _msgSender()),
+            "LedgerV1: must have pauser role to unpause"
+        );
+        _unpause();
+    }
+}
