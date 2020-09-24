e4dcca14b6377dba8a6b965f83a5c24f9c712f6e
Togo Cugu <togo.wolfage.finance@protonmail.com>
2020-09-24 09:38:07 +0530

    Enable, disable, remove and check functionalities added in ContractRegistry.

--- packages/solidity/contracts/ContractRegistry.sol
+++ packages/solidity/contracts/ContractRegistry.sol
@@ -6,13 +6,19 @@ import "./EnhancedAddress.sol";
 contract ContractRegistry is Ownable {
     using EnhancedAddress for address;
 
-    mapping(uint256 => address) public protocols;
+    mapping(uint256 => address) private _addresses;
 
-    event ProtocolRegistered(
+    mapping(uint256 => bool) private _states;
+
+    event ContractRegistered(
         uint256 id,
         address indexed from,
         address indexed to
     );
+
+    event ContractEnabled(uint256 id);
+
+    event ContractDisabled(uint256 id);
 
     constructor(address admin) public {
         transferOwnership(admin);
@@ -20,8 +26,53 @@ contract ContractRegistry is Ownable {
 
     function register(uint256 id, address protocol) external onlyOwner {
         require(protocol.isContract(), "ContractRegistry: not a contract");
-        address oldProtocol = protocols[id];
-        protocols[id] = protocol;
-        emit ProtocolRegistered(id, oldProtocol, protocol);
+        address oldProtocol = _addresses[id];
+        _addresses[id] = protocol;
+        emit ContractRegistered(id, oldProtocol, protocol);
+    }
+
+    function remove(uint256 id) external onlyOwner {
+        require(
+            _addresses[id] != address(0),
+            "ContractRegistry: registry does not exist"
+        );
+        address oldProtocol = _addresses[id];
+        _addresses[id] = address(0);
+        emit ContractRegistered(id, oldProtocol, address(0));
+    }
+
+    function enable(uint256 id) external onlyOwner {
+        require(
+            _addresses[id] != address(0),
+            "ContractRegistry: registry does not exist"
+        );
+        _states[id] = true;
+        emit ContractEnabled(id);
+    }
+
+    function disable(uint256 id) external onlyOwner {
+        require(
+            _addresses[id] != address(0),
+            "ContractRegistry: registry does not exist"
+        );
+        _states[id] = true;
+        emit ContractDisabled(id);
+    }
+
+    function get(uint256 id) external view returns (address) {
+        return _addresses[id];
+    }
+
+    function check(uint256 id) external view returns (bool) {
+        return _states[id];
+    }
+
+    function check(uint256[] calldata ids) external view returns (bool) {
+        for (uint256 i = 0; i < ids.length; i++) {
+            if (!_states[ids[i]]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
